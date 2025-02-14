9a6c10113d1062dfbb569234649680ab6b4c3c93
luqmonac56 <luqmonac56@gmail.com>
2025-02-14 15:39:10 +0100

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,34 @@
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
-import { FormProvider } from "@/contexts/form-context"
-import type React from "react" // Added import for React
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { FormProvider } from "@/contexts/form-context";
+import type React from "react"; // Added import for React
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Conference Ticket Generator",
   description: "Generate your conference ticket",
-    generator: 'v0.dev'
-}
+  generator: "v0.dev",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" className="dark">
       <body>
         <FormProvider>
-          <div className="main-container h-full">
-            <nav className="border-b border-teal-900/20">
-              <div className="container mx-auto px-4 py-4 flex items-center justify-between">
+          <div className="main-container h-full w-full pt-6 ">
+            <nav className="px-4 backdrop-blur-[2px]">
+              <div className="container px-4 py-3 flex items-center justify-between border border-[#197686] rounded-[24px] w-full max-w-[1200px] mx-auto my-0 ">
                 <div className="flex items-center space-x-4">
-                  <span className="text-2xl font-bold text-white">tiez</span>
+                  <img src="/logo.png" alt="logo" />
                 </div>
-                <div className="flex items-center space-x-6">
+
+                <div className="lg:flex items-center hidden  space-x-6">
                   <a href="#" className="text-gray-300 hover:text-white">
                     Events
                   </a>
@@ -37,10 +38,11 @@ export default function RootLayout({
                   <a href="#" className="text-gray-300 hover:text-white">
                     About Project
                   </a>
-                  <button className="bg-white text-black px-4 py-2 rounded-full hover:bg-gray-100">
-                    MY TICKETS →
-                  </button>
                 </div>
+
+                <button className="bg-white text-black px-4 py-3 lg:px-6 lg:py-4 rounded-[12px] hover:bg-gray-100">
+                  MY TICKETS →
+                </button>
               </div>
             </nav>
             <main className="container mx-auto px-4 py-4">{children}</main>
@@ -51,6 +53,5 @@ export default function RootLayout({
   );
 }
 
-
-
-import './globals.css'
+import "./globals.css";
+import Image from "next/image";
